6e89657c6a93f81176892b1511af91e643580ea9
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2022-01-03 23:37:58 +0700

    Add project files.

---
+++ ServerConfigRest_115_Nazih/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+
+namespace ServerConfigRest_115_Nazih
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
